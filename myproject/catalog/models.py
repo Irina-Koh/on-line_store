c54463e0e823f3f2cf22ec4e42a2b9218b93281c
Irina Koh <arina2009.91@mail.ru>
2026-04-13 00:32:42 +0500

    заполнение базы

--- myproject/catalog/models.py
+++ myproject/catalog/models.py
@@ -1,22 +1,7 @@
 from django.db import models
 
 
-class Pr(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Наименование', help_text='Введите наименование товара')
-    description = models.TextField(verbose_name='Описание товара', help_text='Введите описание товара')
-    photo = models.ImageField(upload_to='templates/photo', verbose_name='Фото', help_text='Загрузите фото товара')
-    category = models.CharField(max_length=150, verbose_name='Категория', help_text='Введите категорию')
-    price = models.FloatField(verbose_name='Цена за покупку', help_text='Введите цену')
-    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True )
-    updated_at = models.DateTimeField(verbose_name='Дата создания', auto_now=True)
 
-    def __str__(self):
-        return f'{self.name} {self.description}'
-
-    class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        ordering = ['name', 'category', 'price',]
 
 class Category(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название категории')
@@ -29,3 +14,21 @@ class Category(models.Model):
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
         ordering = ['name',]
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование', help_text='Введите наименование товара')
+    description = models.TextField(verbose_name='Описание товара', help_text='Введите описание товара')
+    photo = models.ImageField(upload_to='templates/photo', verbose_name='Фото', help_text='Загрузите фото товара')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='Категория', help_text='Введите категорию', null=True, blank=True, related_name='products')
+    price = models.FloatField(verbose_name='Цена за покупку', help_text='Введите цену')
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True )
+    updated_at = models.DateTimeField(verbose_name='Дата создания', auto_now=True)
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name', 'category', 'price',]
+
